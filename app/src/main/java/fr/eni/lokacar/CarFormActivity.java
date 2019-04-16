c7a11d644105f99f18e635bb7b66de7f27969991
ALIGAND LINE <line.aligand2017@campus-eni.fr>
2019-04-16 11:56:14 +0200

    fix

--- app/src/main/java/fr/eni/lokacar/CarFormActivity.java
+++ app/src/main/java/fr/eni/lokacar/CarFormActivity.java
@@ -3,17 +3,28 @@ package fr.eni.lokacar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.lokacar.model.CarType;
 
 public class CarFormActivity extends AppCompatActivity {
 
 
-
-    public static final String EXTRA_MODEL = "car_model";
-    public static final String EXTRA_ID = "car_id";
+    public static final String EXTRA_MODEL = "string_car_model";
+    public static final String EXTRA_ID = "string_car_id";
 
 
     EditText tvmodel;
+    Spinner tvtype;
+
+    List voitureType = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +32,35 @@ public class CarFormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_car_form);
 
 
-
         tvmodel = findViewById(R.id.car_model);
+        tvtype = findViewById(R.id.spinner_car_type);
 
-        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_edit_black_24dp);
+        List<CarType> liste_type = new ArrayList<>();
+        liste_type.add(new CarType(0,"Berline"));
+        liste_type.add(new CarType(1,"SUV"));
+        liste_type.add(new CarType(2,"Citadine"));
+        liste_type.add(new CarType(3,"Sportive"));
+
+        for (int i = 0; i < liste_type.size(); i++)
+        {
+            voitureType.add(liste_type.get(i).getLabel());
+        }
+
+        ArrayAdapter ad = new ArrayAdapter<CarType>(this,R.layout.type_spinner,voitureType);
+        tvtype.setAdapter(ad);
+
+
 
         Intent intent = getIntent();
 
-        if(intent.hasExtra(EXTRA_ID))
-        {
-            setTitle("Editer");
+        if (intent.hasExtra(EXTRA_ID)) {
+            setTitle("Modifier");
             tvmodel.setText(intent.getStringExtra(EXTRA_MODEL));
+        } else {
+            setTitle("Ajouter une voiture");
         }
-        else
-        {
-            setTitle("Ajouter");
-        }
+    }
+
+    public void addPhoto(View view) {
     }
 }
--- app/src/main/java/fr/eni/lokacar/ListCarsActivity.java
+++ app/src/main/java/fr/eni/lokacar/ListCarsActivity.java
@@ -30,8 +30,6 @@ public class ListCarsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
                 Intent intent = new Intent(ListCarsActivity.this, CarFormActivity.class);
                 startActivityForResult(intent, REQUEST_CODE_ADD);
             }
